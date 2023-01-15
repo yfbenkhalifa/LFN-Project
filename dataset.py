7efe42cc2bc0d80eff28ab1e966d0c57df01937c
Alessandro Lotta <alessandro.lotta@studenti.unipd.it>
2023-01-15 12:32:40 +0100

    added code for edges and initial code for pagerank

--- dataset.py
+++ dataset.py
@@ -44,20 +44,31 @@ class Dataset:
 
          
     def applyPreprocessing(self):
-        self.df.drop(["Mobility Duration", "Education Level", "Special Needs", "Fewer Opportunities", "GroupLeader",
+        self.df.drop(["Mobility Duration", "Special Needs", "Fewer Opportunities", "GroupLeader",
                      "Sending Organisation Erasmus Code", "Receiving Organisation Erasmus Code"], axis=1, inplace=True)
+        #for x in ["ISCED-6", "ISCED-7", "ISCED-8"]:
+        #    self.df = self.df[self.df["Education Level"].str.contains(x)]
+        self.df.drop("Education Level", axis=1, inplace=True)
         self.df = self.df[self.df["Participant Profile"] == "Learner"]
         self.df = self.df[self.df["Activity (mob)"].str.contains("Student")]
+        self.df = self.df[~self.df['Activity (mob)'].str.contains('traineeship')]
         self.df = self.df.loc[:, ["Sending Organization", "Receiving Organization", "Participants", "Sending Country Code", "Receiving Country Code"]]
         self.df["Sending Organization"] = self.df["Sending Organization"].str.upper()
         self.df["Receiving Organization"] = self.df["Receiving Organization"].str.upper()
         self.df["Sending Country Code"] = self.df["Sending Country Code"].str.upper()
         self.df["Receiving Country Code"] = self.df["Receiving Country Code"].str.upper()
+
     
     def applyFilter(self, column, value, maxrows=None):
         if maxrows is not None:
             self.df = self.df.head(maxrows)
         self.df = self.df[self.df[column] == value]
+
+    def colSize(self):
+        return len(self.df.columns)
+    
+    def rowSize(self):
+        return len(self.df.index)
             
     
                 
